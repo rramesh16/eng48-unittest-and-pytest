9db27ac3c5c0b52b3ae10a29aec8cac951377cf7
Rahavi Ramesh <RRamesh@spartaglobal.com>
2019-12-10 10:16:47 +0000

    More tests add. Additional tests are also passing

--- test_unittest_simplecalc.py
+++ test_unittest_simplecalc.py
@@ -7,13 +7,17 @@ class CalcTest(unittest.TestCase):
 
     def test_add(self):
         self.assertEqual(self.calc.add(2, 4), 6)
+        self.assertEqual(self.calc.add(6, 2), 8)
 
     def test_subtract(self):
         self.assertEqual(self.calc.subtract(4, 2), 2)
+        self.assertEqual(self.calc.subtract(6, 2), 4)
 
     def test_multiply(self):
         self.assertEqual(self.calc.multiply(2, 3), 6)
+        self.assertEqual(self.calc.multiply(6, 2), 12)
 
     def test_divide(self):
         self.assertEqual(self.calc.divide(8, 4), 2)
+        self.assertEqual(self.calc.divide(6, 2), 3)
 
